9f735d498f115c5ee18ed6872b4ed1ca4479fac6
raffe1150 <gadgetproshub@gmail.com>
2026-07-11 22:26:49 +0200

    Update Landing.tsx

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,13 @@
-export default function Landing() {
-  return (
-    <div>
-      Landing Page
-    </div>
-  );
+import landingMarkup from '../components/landing/LandingMarkup';
+import useLandingInteractions from '../components/landing/useLandingInteractions';
+
+interface LandingProps {
+  onNavigate: (path: '/' | '/login' | '/dashboard') => void;
 }
+
+export default function Landing({ onNavigate }: LandingProps) {
+  useLandingInteractions(onNavigate);
+
+  return <div dangerouslySetInnerHTML={{ __html: landingMarkup }} />;
+}
+
